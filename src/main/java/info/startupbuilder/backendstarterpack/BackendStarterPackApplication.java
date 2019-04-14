3b1cb5f4bc8b67383ac5558b517324ffd3985a22
Lyndon Michael Bibera <lyndon.michael.bibera@asia.ing.com>
2019-04-14 10:08:07 +0800

    Generated project from start.spring.io with minimal dependencies for a basic service

---
+++ src/main/java/info/startupbuilder/backendstarterpack/BackendStarterPackApplication.java
@@ -0,0 +1,13 @@
+package info.startupbuilder.backendstarterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStarterPackApplication.class, args);
+	}
+
+}
